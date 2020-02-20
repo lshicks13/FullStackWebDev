79b3873879521418683222b13ab923bc1b95b882
Lauren Hicks <laurenshicks13@gmail.com>
2020-02-19 20:13:54 -0500

    added db connection configs

--- 1_fyyurapp/config.py
+++ 1_fyyurapp/config.py
@@ -7,7 +7,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
+db_type = 'postgresql'
+username = 'postgres'
+password = 'secret123'
+server = 'localhost'
+port = '5432'
+db_name = 'fyyurapp'
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = f'{db_type}://' \
+                                        f'{username}:{password}@' \
+                                        f'{server}:{port}/' \
+                                        f'{db_name}'
